8125babbbd7b38313f5a325f0f2262ce93b60c3e
vvlovsky <slvtrshkv@gmail.com>
2020-10-11 15:20:51 +0200

    Bottom Navigation Bar implementation

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quest_creator/screens/home_screen/home_screen.localizations.dart';
 import 'package:quest_creator/shared_widgets/map_widget/map_widget.dart';
+import 'package:quest_creator/shared_widgets/BottomNavBar.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -10,17 +11,9 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: MapWidget(),
       ),
-      bottomNavigationBar: BottomAppBar(
-        color: Theme.of(context).primaryColor,
-        child: Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(HomeScreenLocalizations.of(context).exampleMessage)
-              )
-            ],
-          ),
-      ),
+      bottomNavigationBar: BottomNavBar(
+
+      )
     );
   }
 
