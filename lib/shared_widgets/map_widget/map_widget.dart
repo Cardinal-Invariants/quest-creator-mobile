7961e737bab452437c33b5ac40c215f356d60088
Dushess0 <ilyabannila@gmail.com>
2020-10-11 15:25:05 +0200

    now you can:  - add markers to map  - move camera to specific position  - detect which marker was clicked by user

--- lib/shared_widgets/map_widget/map_widget.dart
+++ lib/shared_widgets/map_widget/map_widget.dart
@@ -1,26 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
-
-
+import 'dart:developer' as developer;
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'dart:typed_data';
+import 'package:http/http.dart';
 
 class MapWidget extends StatelessWidget {
 
 
   static const String ACCESS_TOKEN = "pk.eyJ1IjoiZHVzaGVzcyIsImEiOiJja2VmcWpneHcwc201MnluNzl3ZDRjNDl1In0.sV8IejZBXjXoUbHgRGeN6w";
+  MapboxMap map;
+  MapboxMapController controller;
+  Function(Symbol) onTapCallback;
 
+
+  MapWidget([Function(Symbol) onTapCallback])
+  {
+    this.onTapCallback=onTapCallback;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: MapboxMap(
-          accessToken: ACCESS_TOKEN,
-          initialCameraPosition:
-          const CameraPosition(target: LatLng(0.0, 0.0)),
-        )
 
+    map= MapboxMap(
 
-       
+      accessToken: ACCESS_TOKEN,
+      initialCameraPosition:
+      const CameraPosition(target: LatLng(0.0, 0.0)),
+      onMapCreated: onMapCreated,
+
     );
+    return new Scaffold(
+        body: this.map
+    );
+  }
+
+  void onMapCreated(MapboxMapController controller) {
+    this.controller = controller;
+    controller.onSymbolTapped.add(onTapCallback);
+  }
+
+
+  int AddMarker(double lat, double long)
+  {
+
+    this.controller.addSymbol(
+      SymbolOptions(
+          geometry: LatLng(lat, long),
+          iconImage: "marker-15",
+          iconSize: 3
+      ),
+    );
+
+    return int.parse(this.controller.symbols.last.id)+1;
+
+
+  }
+  void MoveMap(double lat,double long,double zoom)
+  {
+    CameraUpdate cameraUpdate = CameraUpdate.newLatLngZoom(LatLng(lat, long), zoom);
+    this.controller.moveCamera(cameraUpdate);
   }
 }
